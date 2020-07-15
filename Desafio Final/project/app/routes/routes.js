92b99a0a84513a7fdaa454ac00c2ea9060ef411f
Wellington Ferreira <wellingtonf.souza@outlook.com>
2020-07-14 21:09:10 -0300

    backend ok

--- Desafio Final/project/app/routes/routes.js
+++ Desafio Final/project/app/routes/routes.js
@@ -1,4 +1,10 @@
-const express = require('express');
+const express = require("express");
 const transactionRouter = express.Router();
+const services = require("../services/transactionService.js");
+
+transactionRouter.get("/getAll/", services.findAll);
+transactionRouter.post("/create/", services.create);
+transactionRouter.put("/update/", services.update);
+transactionRouter.delete("/remove/", services.remove);
 
 module.exports = transactionRouter;
--- Desafio Final/project/app/services/transactionService.js
+++ Desafio Final/project/app/services/transactionService.js
@@ -1,8 +1,70 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 // Aqui havia um erro difícil de pegar. Importei como "transactionModel",
 // com "t" minúsculo. No Windows, isso não faz diferença. Mas como no Heroku
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
 // descobrir esse erro :-/
-const TransactionModel = require('../models/TransactionModel');
+const TransactionModel = require("../models/TransactionModel");
+
+const findAll = async (req, res) => {
+  try {
+    const { period } = req.query;
+    const data = await TransactionModel.find({ yearMonth: period });
+    res.send(data);
+  } catch (err) {
+    res.send("Erro: " + err.mensage);
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const Lancamento = new TransactionModel({
+      description: req.body.description,
+      value: req.body.value,
+      category: req.body.category,
+      year: req.body.year,
+      month: req.body.month,
+      day: req.body.day,
+      yearMonth: req.body.yearMonth,
+      yearMonthDay: req.body.yearMonthDay,
+      type: req.body.type,
+    });
+
+    const data = await Lancamento.save();
+    res.send(data);
+  } catch (err) {
+    res.send("Erro: " + err.mensage);
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const data = await TransactionModel.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
+    res.send(data);
+  } catch (err) {
+    res.send("Erro: " + err.mensage);
+  }
+};
+
+const remove = async (req, res) => {
+  const { id } = req.query;
+  try {
+    const data = await TransactionModel.findByIdAndRemove({ _id: id });
+
+    if (!data) {
+      res.send(`Podcast id ${id} nao encontrado`);
+    } else {
+      res.send("Podcast excluido com sucesso");
+    }
+  } catch (err) {
+    res.status(500).send("Erro: " + err.mensage);
+  }
+};
+
+module.exports = { findAll, create, update, remove };
