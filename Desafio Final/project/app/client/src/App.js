4b5e33bc24ebc36ce8d5fba900b88a497e617c8e
Wellington Ferreira <wellingtonf.souza@outlook.com>
2020-07-15 23:12:06 -0300

    add input period center

--- Desafio Final/project/app/client/src/App.js
+++ Desafio Final/project/app/client/src/App.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import css from "./css/style.module.css";
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [startDate, setStartDate] = useState(new Date());
+  const minDate = new Date(2019, 11, 30);
+  const maxDate = new Date(2021, 11, 31);
+  const handleDate = (date) => {
+    setStartDate(date);
+    // console.log(date.toISOString().substring(0, 7));
+  };
+  return (
+    <div>
+      <h3 className="center">Controle Financeiro Pessoal</h3>
+      <div className={css.DatePickerSelectPeriod}>
+        <DatePicker
+          selected={startDate}
+          onChange={handleDate}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          minDate={minDate}
+          maxDate={maxDate}
+          className={css.DatePickerSelectPeriodInput}
+        />
+      </div>
+    </div>
+  );
 }
